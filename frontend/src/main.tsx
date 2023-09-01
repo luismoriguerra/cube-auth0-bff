31f7f8b752620d4204d979ea474f9b71eb3b00da
Luis Mori Guerra <luismorith@gmail.com>
2023-09-01 17:21:53 -0500

    feat: adding auth0
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-9k50c76d.us.auth0.com"
+      clientId="642LkVJ5KzSoH8gThpU18xKFf03adhjN"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
